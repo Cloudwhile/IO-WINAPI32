a40ab24e353acbdb36f864dde3af290351d6399b
Cloudwhile <86512509+Cloudwhile@users.noreply.github.com>
2021-10-23 18:36:06 +0800

    Update IO Function.cpp

--- IO Function.cpp
+++ IO Function.cpp
@@ -1,1 +1,52 @@
 
+include<iostream>
+#include<string.h>
+#include<windows.h>
+using namespace std;
+void ain(){
+float a,b,c;
+char nu;
+cout<<"Calculate(a?b[1+1]):";
+cin>>a>>nu>>b;
+while(1){
+switch (nu){
+case '+':
+    c = a + b;
+    cout<<a<<nu<<b<<"="<<c<<endl;
+    break;
+case '-':
+    c = a - b;
+    cout<<a<<nu<<b<<"="<<c<<endl;
+    break;
+case '*':
+    c = a * b;
+    cout<<a<<nu<<b<<"="<<c<<endl;
+    break;
+case '/':
+    if b == 0{
+    cout<<"the second number must not be zero!"<<endl;
+}
+else {
+    c = a / b
+    cout<<a<<nu<<b<<"="<<c<<endl;
+}
+    break;
+}
+default:
+    cout<<"Please enter '+','-','*','//'."<<endl;
+    break;
+}
+}
+int main (){
+int v,d = MessageBox(NULL,TEXT("functions are prepared"),TEXT("Come from process"),MB_YESNO);
+if(d == IDYES){
+    cout<<"Please choose a functions to run"<<endl;
+    cout<<"Here only a functions [1](calculate):";
+    cin>>v;
+    switch(v){
+    case '1':
+         ain();
+}
+}
+return 0;
+}
